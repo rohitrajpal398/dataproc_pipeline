945e7694f0c4010ea4437627c7e5760593ffc9c4
Rohit Rajpal <rohit.rajpal@nihilent.com>
2025-05-12 16:29:41 +0530

    added the changes

---
+++ dataproc_pipeline_project/dags/dataproc_job_dag.py
@@ -0,0 +1,72 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocCreateClusterOperator,
+    DataprocSubmitJobOperator,
+    DataprocDeleteClusterOperator
+)
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+
+PROJECT_ID = "airy-actor-457907-a8"
+REGION = "us-central1"
+CLUSTER_NAME = "demo-cluster"
+BUCKET_NAME = "sample-data-for-pract1"
+BQ_TEMP_BUCKET = "sample-data-for-pract1"
+
+CLUSTER_CONFIG = {
+    "master_config": {
+        "num_instances": 1,
+        "machine_type_uri": "n1-standard-2"
+    },
+    "worker_config": {
+        "num_instances": 2,
+        "machine_type_uri": "n1-standard-2"
+    }
+}
+
+PYSPARK_JOB = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "pyspark_job": {
+        "main_python_file_uri": f"gs://sample-data-for-pract1/spark_jobs/main_job.py",
+        "jar_file_uris": ["gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar"]
+    },
+}
+
+default_args = {
+    "start_date": days_ago(1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    "dataproc_cluster_job_lifecycle",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
+
+    create_cluster = DataprocCreateClusterOperator(
+        task_id="create_cluster",
+        project_id=PROJECT_ID,
+        cluster_config=CLUSTER_CONFIG,
+        region=REGION,
+        cluster_name=CLUSTER_NAME
+    )
+
+    submit_job = DataprocSubmitJobOperator(
+        task_id="submit_spark_job",
+        job=PYSPARK_JOB,
+        region=REGION,
+        project_id=PROJECT_ID
+    )
+
+    delete_cluster = DataprocDeleteClusterOperator(
+        task_id="delete_cluster",
+        project_id=PROJECT_ID,
+        cluster_name=CLUSTER_NAME,
+        region=REGION,
+        trigger_rule="all_done"
+    )
+
+    create_cluster >> submit_job >> delete_cluster	
---
+++ dataproc_pipeline_project/spark_jobs/main_job.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+
+def main():
+    spark = Spark Session.builder.appName("SalesPipeline").getOrCreate()
+    df = spark.read.csv("gs://sample-data-for-pract1/orders.csv", header=True, inferSchema=True)
+    df = df.withColumn("total", df.quantity * df.price)
+    df.write.format("bigquery") \
+        .option("table", "airy-actor-457907-a8.sales_dataset.sales_summary") \
+        .option("temporaryGcsBucket", "sample-data-for-pract1") \
+        .mode("overwrite") \
+        .save()
+
+if __name__ == "__main__":
+    main()
